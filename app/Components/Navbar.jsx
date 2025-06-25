c9f3c88059a927d683fc09052febc7976c504db5
Farhan Yousafzai <coderwithferry@gmail.com>
2025-06-26 00:32:26 +0500

    Clerk Authectications

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,46 +1,36 @@
 'use client'
 
-import {auth, signIn, signOut } from '@/auth'
+import { SignInButton, SignOutButton, UserButton, useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
 
-const Navbar = async() => {
-
-
-
-    // You need to fetch the session client-side since 'auth()' is a server-only function
-  const session = await auth()
-
-
+const Navbar = () => {
+  const { isSignedIn, user } = useUser()
 
   return (
-    <header className='px-5 py-5 bg-white shadow-sm'>
+    <header className='h-[70px] p-4 bg-white shadow-sm '>
       <nav className='flex items-center justify-between'>
         <Link href='/'>
-          <Image src='/logo.png' alt='logo' width={100} height={100} />
+       <span className='text-[1.3rem] font-bold uppercase'>Next<span className='text-purple-500'>Idea</span></span>
         </Link>
 
         <div className='flex items-center gap-4'>
-          {session && session.user ? (
+          {isSignedIn ? (
             <>
               <Link href='/startup/create'>
-                <button>Create Startup</button>
+                <button className='bg-purple-500 text-white px-4 py-2 rounded-md cursor-pointer md:flex hidden'>Create Startup</button>
               </Link>
 
-              <button onClick={() => signOut()}>Logout</button>
+              <UserButton showName={true} />
 
-              <Link href={`/profile/${session.user.id}`}>
-                {session.user.name}
-              </Link>
+              
             </>
           ) : (
-            <button
-              onClick={async() => await signIn('github')}
-              className='bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer'
-            >
-              Login with GitHub
-            </button>
+            <SignInButton mode='modal'>
+              <button className=' text-white px-4 py-2 rounded-md cursor-pointer'>
+                Login with Clerk
+              </button>
+            </SignInButton>
           )}
         </div>
       </nav>
