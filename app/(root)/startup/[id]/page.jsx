19d251d66fbd749fbd382ed395a95ceb4dc6687d
Farhan Yousafzai <coderwithferry@gmail.com>
2025-07-01 13:13:34 +0500

    Nimaz Time

--- app/(root)/startup/[id]/page.jsx
+++ app/(root)/startup/[id]/page.jsx
@@ -6,10 +6,15 @@ const page = async ({ params }) => {
   const data = await client.fetch(STARTUP_BY_ID_QUERY, { id: params.id });
 
   return (
-    <div>
-      <h2>This is the startup id: {data._id}</h2>
-      <p>{data.title}</p>
+    <>
+     <div className='w-full bg-pink-400 !min-h-[300px] pattern flex justify-center items-center flex-col py-10 px-6 pattern  '>
+
+
+
+<h1 className=' uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5 rounded-md'>Power of connection</h1>
+
     </div>
+    </>
   );
 };
 
---
+++ app/(root)/startup/create/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <div className='w-full bg-pink-400 !min-h-[300px] pattern flex justify-center items-center flex-col py-10 px-6 pattern  '>
+
+
+  <h1 className=' uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5 rounded-md'>Submit your startup</h1>
+ 
+      </div>
+
+      
+    </>
+  )
+}
+
+export default page
